7861c87b8ac181c4878190f9112c19028ea63db1
Mateo Spatola <mateospatola@hotmail.com>
2026-07-31 19:58:16 -0300

    Inicializar proyecto con Spring Initializr

---
+++ api/src/test/java/com/mateospatola/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mateospatola.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
