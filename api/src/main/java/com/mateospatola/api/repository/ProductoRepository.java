f94721f0f957da3306c43f636b9a8ffc40539294
Mateo Spatola <mateospatola@hotmail.com>
2026-08-03 19:20:38 -0300

    Crear repositorios

---
+++ api/src/main/java/com/mateospatola/api/repository/ProductoRepository.java
@@ -0,0 +1,9 @@
+package com.mateospatola.api.repository;
+
+import com.mateospatola.api.model.Producto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductoRepository extends JpaRepository<Producto, Long> {
+}
---
+++ api/src/main/java/com/mateospatola/api/repository/VentaRepository.java
@@ -0,0 +1,9 @@
+package com.mateospatola.api.repository;
+
+import com.mateospatola.api.model.Venta;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VentaRepository extends JpaRepository<Venta, Long> {
+}
