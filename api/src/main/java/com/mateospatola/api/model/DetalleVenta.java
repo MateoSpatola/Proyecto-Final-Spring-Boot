9a5811b9dee56ee259a1a632b0f309ff0677ba66
Mateo Spatola <mateospatola@hotmail.com>
2026-08-08 16:16:04 -0300

    Agregar calculo del subtotal en DetalleVenta

--- api/src/main/java/com/mateospatola/api/model/DetalleVenta.java
+++ api/src/main/java/com/mateospatola/api/model/DetalleVenta.java
@@ -22,4 +22,8 @@ public class DetalleVenta {
     private Producto producto;
     private Integer cantidad;
     private Double precioUnitario;
+
+    public Double calcularSubtotal() {
+        return cantidad * precioUnitario;
+    }
 }
