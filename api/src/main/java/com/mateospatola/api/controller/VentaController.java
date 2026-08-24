146749c19eed50cad411242692a332cb7b8567dc
Mateo Spatola <mateospatola@hotmail.com>
2026-08-24 19:10:23 -0300

    Implementar controlador de Venta

---
+++ api/src/main/java/com/mateospatola/api/controller/VentaController.java
@@ -0,0 +1,51 @@
+package com.mateospatola.api.controller;
+
+import com.mateospatola.api.dto.venta.VentaRequestDTO;
+import com.mateospatola.api.dto.venta.VentaResponseDTO;
+import com.mateospatola.api.service.IVentaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/ventas")
+public class VentaController {
+
+    @Autowired
+    private IVentaService ventaService;
+
+
+    @GetMapping
+    public ResponseEntity<List<VentaResponseDTO>> getAll() {
+        return ResponseEntity.ok(ventaService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VentaResponseDTO> getById(@PathVariable Long id) {
+        return ResponseEntity.ok(ventaService.getById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<VentaResponseDTO> create(@RequestBody VentaRequestDTO ventaRequestDTO) {
+        VentaResponseDTO created = ventaService.create(ventaRequestDTO);
+        URI location = URI.create("/api/ventas/" + created.getId());
+        return ResponseEntity.created(location).body(created);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<VentaResponseDTO> update(@PathVariable Long id, @RequestBody VentaRequestDTO ventaRequestDTO) {
+        VentaResponseDTO updated = ventaService.update(id, ventaRequestDTO);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        ventaService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+}
