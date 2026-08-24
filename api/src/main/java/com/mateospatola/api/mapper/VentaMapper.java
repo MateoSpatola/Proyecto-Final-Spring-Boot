40869c92d4c6924d604a306e50814f68da8e6039
Mateo Spatola <mateospatola@hotmail.com>
2026-08-24 19:09:40 -0300

    Agregar mapper de Venta

---
+++ api/src/main/java/com/mateospatola/api/mapper/VentaMapper.java
@@ -0,0 +1,43 @@
+package com.mateospatola.api.mapper;
+
+import com.mateospatola.api.dto.venta.DetalleVentaResponseDTO;
+import com.mateospatola.api.dto.venta.VentaResponseDTO;
+import com.mateospatola.api.model.DetalleVenta;
+import com.mateospatola.api.model.Venta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VentaMapper {
+
+    public static VentaResponseDTO toResponseDTO(Venta entity) {
+        VentaResponseDTO ventaResponseDTO = new VentaResponseDTO();
+        ventaResponseDTO.setId(entity.getId());
+        ventaResponseDTO.setFecha(entity.getFecha());
+        ventaResponseDTO.setTotal(entity.getTotal());
+        ventaResponseDTO.setClienteId(entity.getCliente().getId());
+
+        List<DetalleVentaResponseDTO> detallesDTO = new ArrayList<>();
+        for (DetalleVenta detalle : entity.getDetalles()) {
+            DetalleVentaResponseDTO detalleDTO = new DetalleVentaResponseDTO();
+            detalleDTO.setId(detalle.getId());
+            detalleDTO.setVentaId(detalle.getVenta().getId());
+            detalleDTO.setProductoId(detalle.getProducto().getId());
+            detalleDTO.setCantidad(detalle.getCantidad());
+            detalleDTO.setPrecioUnitario(detalle.getPrecioUnitario());
+            detalleDTO.setSubtotal(detalle.calcularSubtotal());
+            detallesDTO.add(detalleDTO);
+        }
+        ventaResponseDTO.setDetallesDTO(detallesDTO);
+        return ventaResponseDTO;
+    }
+
+    public static List<VentaResponseDTO> toResponseDTOList(List<Venta> entities) {
+        List<VentaResponseDTO> responseDTOList = new ArrayList<>();
+        for (Venta entity : entities) {
+            responseDTOList.add(VentaMapper.toResponseDTO(entity));
+        }
+        return responseDTOList;
+    }
+
+}
